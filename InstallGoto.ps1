70ab934b5099e3abd6a1e619a2d7eb82fef06ca5
Ranamzes <ranamzes@yandex.ru>
2024-04-15 19:47:43 +0600

    install update

--- InstallGoto.ps1
+++ InstallGoto.ps1
@@ -20,7 +20,7 @@ Load-Aliases
 # Check if the profile already contains a reference to GotoFunction.ps1 and add if not
 if (-Not (Select-String -Path $PROFILE -Pattern "GotoFunction.ps1" -Quiet)) {
     Add-Content -Path $PROFILE -Value $profileContent
-    Write-Host "Goto function has been added to your PowerShell profile."
+    Write-Host "Goto function has been added to your PowerShell profile. Please reload your PowerShell."
 } else {
     Write-Host "Goto function is already registered in your PowerShell profile."
 }
